5e50288d14677e938dcdbd22ce4f5f319f68e101
lws <1172953868@qq.com>
2019-06-25 14:33:03 +0800

    更新main方法

--- idea-git-test/src/main/java/Test.java
+++ idea-git-test/src/main/java/Test.java
@@ -1,8 +1,14 @@
+
+
+import java.io.File;
+import java.io.IOException;
+
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("ddd");
+        System.out.println("第二次提交");
+        System.out.println("哈哈");
 
     }
 }
